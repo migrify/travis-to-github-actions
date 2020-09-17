0541640355a40cd4759b3904ea79ec23ebb871ce
Tomas Votruba <tomas.vot@gmail.com>
2020-09-17 14:30:59 +0200

    [TravisToGithubActions] Rename package from generic CI name (#170)

---
+++ tests/TravisToGithubActionsConverter/TravisToGithubActionsConverterTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\TravisToGithubActions\Tests\TravisToGithubActionsConverter;
+
+use Iterator;
+use Migrify\TravisToGithubActions\HttpKernel\TravisToGithubActionsKernel;
+use Migrify\TravisToGithubActions\TravisToGithubActionsConverter;
+use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
+use Symplify\EasyTesting\StaticFixtureSplitter;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class TravisToGithubActionsConverterTest extends AbstractKernelTestCase
+{
+    /**
+     * @var TravisToGithubActionsConverter
+     */
+    private $travisToGithubActionsConverter;
+
+    protected function setUp(): void
+    {
+        $this->bootKernel(TravisToGithubActionsKernel::class);
+        $this->travisToGithubActionsConverter = self::$container->get(TravisToGithubActionsConverter::class);
+    }
+
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fixtureFileInfo): void
+    {
+        $inputAndExpectedFileInfo = StaticFixtureSplitter::splitFileInfoToLocalInputAndExpectedFileInfos(
+            $fixtureFileInfo
+        );
+
+        $convertedFileContent = $this->travisToGithubActionsConverter->convert(
+            $inputAndExpectedFileInfo->getInputFileInfo()
+        );
+
+        $this->assertSame(
+            $inputAndExpectedFileInfo->getExpectedFileInfo()->getContents(),
+            $convertedFileContent,
+            $fixtureFileInfo->getRelativeFilePathFromCwd()
+        );
+    }
+
+    public function provideData(): Iterator
+    {
+        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/Fixture', '*.yml');
+    }
+}
